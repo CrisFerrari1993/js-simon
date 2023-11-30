b77749b4790475b7022448a1a8491ed49642c431
Cristopher Ferrari <ferraricristopher93@gmail.com>
2023-11-30 16:31:26 +0100

    JS Commentazione esercizio

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*
+Descrizione:
+Visualizzare in pagina 5 numeri casuali.
+Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, i numeri che ha visto precedentemente.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+Il layout lo decidete voi.
+Non perdetevi solo nei suoi dettagli da subito, ma date priorità a farlo funzionare.
+Poi se volete lo abbellite quanto volete, quando funziona tutto.
+*/
